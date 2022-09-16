4fca3fd6924da5bef2c5d57bb18cbc444c2474ed
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-16 22:30:13 +0300

    Create win way and create array for each user

--- scripts.js
+++ scripts.js
@@ -10,42 +10,51 @@ const div_7 =document.getElementById('div_7');
 const div_8 =document.getElementById('div_8');
 const div_9 =document.getElementById('div_9');
 let counter=0;
+let array_red=[];
+let array_yellow=[];
+const array = ["123","456","789","147","258","369","159","357"];
 
-const addBackground = (div)=>{
-counter++;
-if(counter%2!=0){
-    div.classList.add("red-circle");
-}else{
-    div.classList.add("yellow-circle");
+
+const checkWinner = () =>{
+
+}
+const addBackground = (div,num)=>{
+if(!(div.classList.contains("red-circle") || div.classList.contains("yellow-circle"))){
+    counter++;
+    if(counter%2!=0){
+        div.classList.add("red-circle");
+        array_red.push(num);
+    }else{
+        div.classList.add("yellow-circle");
+        array_yellow.push(num);
+    }
+    checkWinner();
 }
 }
-
-
-
 div_1.addEventListener('click',function(){
-    addBackground(div_1);
+    addBackground(div_1,"1");
 });
 div_2.addEventListener('click',function(){
-    addBackground(div_2);
+    addBackground(div_2,"2");
 });
 div_3.addEventListener('click',function(){
-    addBackground(div_3);
+    addBackground(div_3,"3");
 });
 div_4.addEventListener('click',function(){
-    addBackground(div_4);
+    addBackground(div_4,"4");
 });
 div_5.addEventListener('click',function(){
-    addBackground(div_5);
+    addBackground(div_5,"5");
 });
 div_6.addEventListener('click',function(){
-    addBackground(div_6);
+    addBackground(div_6,"6");
 });
 div_7.addEventListener('click',function(){
-    addBackground(div_7);
+    addBackground(div_7,"7");
 });
 div_8.addEventListener('click',function(){
-    addBackground(div_8);
+    addBackground(div_8,"8");
 });
 div_9.addEventListener('click',function(){
-    addBackground(div_9);
+    addBackground(div_9,"9");
 });
