6b9fced5a7c5dd8173e3e4adad6c461341e37dd6
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-16 23:51:38 +0300

    Clean code and write some comments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Define all elements
 const result_yellow =document.getElementById('result_yellow');
 const result_red =document.getElementById('result_red');
 const div_1 =document.getElementById('div_1');
@@ -9,6 +10,7 @@ const div_6 =document.getElementById('div_6');
 const div_7 =document.getElementById('div_7');
 const div_8 =document.getElementById('div_8');
 const div_9 =document.getElementById('div_9');
+// Define all attributes to be used later on
 let counter=0;
 let array_red=[];
 let array_yellow=[];
@@ -18,6 +20,8 @@ const array = ["123","456","789","147","258","369","159","357",
                 "231","564","897","471","582","693","591","573"];
 let score_red=0;
 let score_yellow=0;
+
+// Restart the game, so all backgrounds we return back as initial case.
 const restartGame=()=>{
     array_red=[];
     array_yellow=[];
@@ -31,16 +35,21 @@ const restartGame=()=>{
         }
     }
 }
+
+// Add 10 bonus to red 
 const addScoreRed=()=>{
     score_red+=10;
     result_red.innerText=`Result-red: ${score_red}`;
     restartGame();
 }
+// Add 10 bonus to yellow 
 const addScoreYellow=()=>{
     score_yellow+=10;
     result_yellow.innerText=`Result-yellow: ${score_yellow}`;
     restartGame();
 }
+
+//Here, we are checking the winner after the length of picking up is 3, so, at least the user must pick up 3 positions in order to win.
 const checkWinner = () =>{
     if(array_red.length>=3){
         let way='';
@@ -53,7 +62,6 @@ const checkWinner = () =>{
             }
         }
     }
-
     if(array_yellow.length>=3){
         let way='';
         for(let i=array_yellow.length-3;i<array_yellow.length;i++){
@@ -66,6 +74,7 @@ const checkWinner = () =>{
         }
     }
 }
+// Add background to the clicked position.
 const addBackground = (div,num)=>{
 if(!(div.classList.contains("red-circle") || div.classList.contains("yellow-circle"))){
     counter++;
@@ -82,30 +91,39 @@ if(!(div.classList.contains("red-circle") || div.classList.contains("yellow-circ
     }
 }
 }
+// Listener for div1
 div_1.addEventListener('click',function(){
     addBackground(div_1,"1");
 });
+// Listener for div2
 div_2.addEventListener('click',function(){
     addBackground(div_2,"2");
 });
+// Listener for div3
 div_3.addEventListener('click',function(){
     addBackground(div_3,"3");
 });
+// Listener for div4
 div_4.addEventListener('click',function(){
     addBackground(div_4,"4");
 });
+// Listener for div5
 div_5.addEventListener('click',function(){
     addBackground(div_5,"5");
 });
+// Listener for div6
 div_6.addEventListener('click',function(){
     addBackground(div_6,"6");
 });
+// Listener for div7
 div_7.addEventListener('click',function(){
     addBackground(div_7,"7");
 });
+// Listener for div8
 div_8.addEventListener('click',function(){
     addBackground(div_8,"8");
 });
+// Listener for div9
 div_9.addEventListener('click',function(){
     addBackground(div_9,"9");
 });
